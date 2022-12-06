7c20c13f0b57e815d6e7202b41efae4d555f384a
looh16 <langacustodio@gmail.com>
2022-12-06 19:48:25 +0200

    fix lint errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,8 +1,11 @@
-class Api::V1::MessagesController < ApplicationController
-
-# GET /users
-def index
-    @messages = Message.order('RANDOM()').first
-    render json: @messages
-   end
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      # GET /users
+      def index
+        @messages = Message.order('RANDOM()').first
+        render json: @messages
+      end
+    end
+  end
 end
